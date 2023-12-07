edd74a817cdefc4656e49ac71956e29de256c712
connor anderson <anderson5459@sps81.org>
2023-12-07 08:20:20 -0800

    added margin to text on homepage

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,4 +1,4 @@
-var img_array = ["../images/silly-little-guy.jpeg", "../images/placeholder2.png"];
+var img_array = ["url(../images/silly-little-guy.jpeg)", "url(../images/placeholder2.png)"];
 
 var index = 0;
 
@@ -13,13 +13,13 @@ function upindex() {
 function downindex() {
     index--;
     if (index < 0) {
-        index = img_array.length();
+        index = img_array.length() - 1;
     }
     img_change();
 }
 
 function img_change() {
-    document.getElementById("slideshow").style.backgroundImage = "url(" + img_array[index] + ")";
+    document.getElementById("slideshow").style.backgroundImage = img_array[index];
 }
 
 setInterval(upindex, 10000);
