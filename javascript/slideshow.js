e38a3761f7866b5af558b6c899a23a30828ef2b1
ciander <ciander@proton.me>
2023-12-06 18:49:10 -0800

    added slideshow javascript

---
+++ javascript/slideshow.js
@@ -0,0 +1,23 @@
+var img_array = ["../images/silly-little-guy.jpeg", "../images/placeholder2.png"];
+
+var index = 0;
+
+function upindex() {
+    index++;
+    if (index > img_array.length()) {
+        index = 0;
+    }
+    img_change();
+}
+
+function downindex() {
+    index--;
+    if (index < 0) {
+        index = img_array.length();
+    }
+    img_change();
+}
+
+function img_change() {
+    document.getElementById("slideshow").style.backgroundImage = "url(" + img_array[index] + ")";
+}
