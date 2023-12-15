993eceafafc12548d7620c7a6d5cfc4cc9617641
jason <green1146@sps81.org>
2023-12-15 11:13:02 -0800

    stuff

--- javascript/random-link.js
+++ javascript/random-link.js
@@ -1,13 +1,11 @@
+var sites = [
+  'http://www.google.com',
+  'http://www.stackoverflow.com',
+  'http://www.example.com',
+  'http://www.youtube.com'
+];
 
-function openSite() {
-  var links = [
-    "../Reciepe-temp.html",
-    "../Home-made-chips.html",
-    "../calender.html",
-    "../Chiken-Katsu-and-Rice.html"]
-
-    function randomSite() {
-      var i = parseInt(Math.random() * sites.length);
-      location.href = sites[i];
-  }
+function randomSite() {
+  var i = parseInt(Math.random() * sites.length);
+  location.href = sites[i];
 }
