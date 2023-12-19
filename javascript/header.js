9348a2a00425b30aa8f3ced0d3b8ddf5845bd4f1
Lucian <Sloan4016@sps81.org>
2023-12-19 11:08:57 -0800

    club 2

--- javascript/header.js
+++ javascript/header.js
@@ -4,9 +4,9 @@ document.write('\
         <img id = "logo" src="https://www.spokaneschools.org/SPS/Schoolwires.2015Files/3.1.15/images/logos/northcentral.png">\
 \
         <div id = "nav">\
-            <a href="#" onclick="randomSite();">Random</a>\
-            <a href="./calender.html">calender</a>\
             <a href="./Home-page.html">home</a>\
+            <a href="./calender.html">calender</a>\
+            <a href="#" onclick="randomSite();">Random</a>\
         </div>\
     </header>\
 ');
